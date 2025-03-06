4c751c54187f0aa6f9a44370d9cf08e254be6be3
bgclara1 <clara.bollengandolfo@gmail.com>
2025-03-06 13:56:09 +0000

    parse cmd input

--- simulation.cpp
+++ simulation.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char *argv[]) {              //read cmd args w main params.
     int i = 0;
 
-    map<string, map<string, vector<double>>> testCaseDict;
+    map<string, map<string, vector<double> > > testCaseDict;
     testCaseDict["-ic-one"] = {
         {"time", {1.0}},
         {"numParticles", {1}},
@@ -77,17 +77,43 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
     };
     
 
+    double time;
+    int numParticles;
+    vector<double> x, y, z, u, v, w, type;
+
     // test reading in args properly
     while (i < argc) {  //argc num args provided
         cout << "Argument " << i + 1 << ": " << argv[i]
              << endl;
-        if (testCaseDict.find(argv[i]) != testCaseDict.end()) {
+        string key(argv[i]);
 
-        }
 
+        if (testCaseDict.find(key) != testCaseDict.end()) {
+            time = testCaseDict[key]["time"][0];
+            numParticles = testCaseDict[key]["numParticles"][0];
+            x = testCaseDict[key]["x"];
+            y = testCaseDict[key]["y"];
+            z = testCaseDict[key]["z"];
+            u = testCaseDict[key]["u"];
+            v = testCaseDict[key]["v"];
+            w = testCaseDict[key]["w"];
+            type = testCaseDict[key]["type"];
         }
-        i++;
     
+        i++;
+    }
+
+    cout << time << endl;
+    cout << numParticles << endl;
+    cout << x[0] << endl;
+    cout << y[0] << endl;
+    cout << z[0] << endl;
+    cout << u[0] << endl;
+    cout << v[0] << endl;
+    cout << w[0] << endl;
+    cout << type[0] << endl;
 
     return 0;
+  
+
 }
