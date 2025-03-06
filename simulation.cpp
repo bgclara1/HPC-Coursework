7747143aa36c6e19b6f97765fa114678b0416907
bgclara1 <clara.bollengandolfo@gmail.com>
2025-03-06 15:28:26 +0000

    added help function

--- simulation.cpp
+++ simulation.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
     
 
     map<string, map<string, vector<double> > > testCaseDict;
-    testCaseDict["-ic-one"] = {
+    testCaseDict["--ic-one"] = {
         {"time", {1.0}},
         {"numParticles", {1}},
         {"x", {10.0}},
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
         {"w", {0.0}},
         {"type", {0}}
     };
-    testCaseDict["-ic-one-vel"] = {
+    testCaseDict["--ic-one-vel"] = {
         {"time", {20.0}},
         {"numParticles", {1}},
         {"x", {10.0}},
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
         {"w", {1.0}},
         {"type", {0}}
     };
-    testCaseDict["-ic-two"] = {
+    testCaseDict["--ic-two"] = {
         {"time", {50.0}},
         {"numParticles", {2}},
         {"x", {8.5, 11.5}},
@@ -56,7 +56,7 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
         {"w", {0.0, 0.0}},
         {"type", {0, 0}}
     };
-    testCaseDict["-ic-two-pass1"] = {
+    testCaseDict["--ic-two-pass1"] = {
         {"time", {50.0}},
         {"numParticles", {2}},
         {"x", {8.5, 11.5}},
@@ -67,7 +67,7 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
         {"w", {0.0, 0.0}},
         {"type", {0, 0}}
     };
-    testCaseDict["-ic-two-pass2"] = {
+    testCaseDict["--ic-two-pass2"] = {
         {"time", {50.0}},
         {"numParticles", {2}},
         {"x", {8.5, 11.5}},
@@ -78,7 +78,7 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
         {"w", {0.0, 0.0}},
         {"type", {0, 0}}
     };
-    testCaseDict["-ic-two-pass3"] = {
+    testCaseDict["--ic-two-pass3"] = {
         {"time", {50.0}},
         {"numParticles", {2}},
         {"x", {8.5, 11.5}},
@@ -100,21 +100,21 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
         cout << "Argument " << i + 1 << ": " << argv[i]
              << endl;
 
-        if (string(argv[i]) == "-Lx") {
+        if (string(argv[i]) == "--Lx") {
             cout << "hey " << endl;
             Lx = stod(argv[i+1]);
             cout << argv[i+1] << endl;
-        } else if (string(argv[i]) == "-Ly") {
+        } else if (string(argv[i]) == "--Ly") {
             Ly = stod(argv[i+1]);
-        } else if (string(argv[i]) == "-Lz") {
+        } else if (string(argv[i]) == "--Lz") {
             Lz = stod(argv[i+1]);
-        } else if (string(argv[i]) == "-T") {
+        } else if (string(argv[i]) == "--T") {
             time = stod(argv[i+1]);
             timeProvided = true;
-        } else if (string(argv[i]) == "-N") {
+        } else if (string(argv[i]) == "--N") {
             numParticles = stod(argv[i+1]);
             nProvided = true;
-        } else if (string(argv[i]) == "-ic-random") {
+        } else if (string(argv[i]) == "--ic-random") {
             icRandomChosen = true;
         } else if (testCaseDict.find(string(argv[i])) != testCaseDict.end()) {
             string key(argv[i]);
@@ -128,7 +128,25 @@ int main(int argc, char *argv[]) {              //read cmd args w main params.
             w = testCaseDict[key]["w"];
             type = testCaseDict[key]["type"];
             testCase = true;
-        } 
+        } else if (string(argv[i]) == "--help") {
+            cout << "--help" << "                            " << "Print available options" << endl;
+            cout << "--Lx arg (=20)" << "                    " << "x length (Angstroms)" << endl;
+            cout << "--Ly arg (=20)" << "                    " << "y length (Angstroms)" << endl;
+            cout << "--Lz arg (=20)" << "                    " << "z length (Angstroms)" << endl;
+            cout << "--dt arg (=0.001)" << "                 " << "Time-step" << endl;
+            cout << "--T" << "                               " << "Final Time" << endl;
+            cout << "--ic-one" << "                          " << "Initial condition: one stationary particle" << endl;
+            cout << "--ic-one-vel" << "                      " << "Initial condition: one moving particle" << endl;
+            cout << "--ic-two" << "                          " << "Initial condition: two bouncing particles" << endl;
+            cout << "--ic-two-pass1" << "                    " << "Initial condition: two passing particles" << endl;
+            cout << "--ic-two-pass2" << "                    " << "Initial condition: two passing particles close" << endl;
+            cout << "--ic-two-pass3" << "                    " << "Initial condition: two passing particles close, heavy" << endl;
+            cout << "--ic-random" << "                       " << "Initial condition: N random particles" << endl;
+            cout << "--percent-type1 arg (=10)" << "         " << "Percentage of type 1 particles with random IC" << endl;
+            cout << "--N arg" << "                           " << "Number of particles to spawn with random IC" << endl;
+            cout << "--temp arg" << "                        " << "Temperature (degrees Kelvin)" << endl;
+
+        }
 
         i++;
     }
