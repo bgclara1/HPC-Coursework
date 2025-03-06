749e92c9b5dfca771ac103371986cfa174b41e2a
bgclara1 <clara.bollengandolfo@gmail.com>
2025-03-06 13:01:52 +0000

    initialised test cases as a map

--- simulation.cpp
+++ simulation.cpp
@@ -4,20 +4,90 @@
 
 using namespace std;
 
+
 int main(int argc, char *argv[]) {              //read cmd args w main params.
     int i = 0;
+
+    map<string, map<string, vector<double>>> testCaseDict;
+    testCaseDict["-ic-one"] = {
+        {"time", {1.0}},
+        {"numParticles", {1}},
+        {"x", {10.0}},
+        {"y", {10.0}},
+        {"z", {10.0}},
+        {"u", {0.0}},
+        {"v", {0.0}},
+        {"w", {0.0}},
+        {"type", {0}}
+    };
+    testCaseDict["-ic-one-vel"] = {
+        {"time", {20.0}},
+        {"numParticles", {1}},
+        {"x", {10.0}},
+        {"y", {10.0}},
+        {"z", {10.0}},
+        {"u", {5.0}},
+        {"v", {2.0}},
+        {"w", {1.0}},
+        {"type", {0}}
+    };
+    testCaseDict["-ic-two"] = {
+        {"time", {50.0}},
+        {"numParticles", {2}},
+        {"x", {8.5, 11.5}},
+        {"y", {10.0, 10.0}},
+        {"z", {10.0, 10.0}},
+        {"u", {0.0, 0.0}},
+        {"v", {0.0, 0.0}},
+        {"w", {0.0, 0.0}},
+        {"type", {0, 0}}
+    };
+    testCaseDict["-ic-two-pass1"] = {
+        {"time", {50.0}},
+        {"numParticles", {2}},
+        {"x", {8.5, 11.5}},
+        {"y", {11.5, 8.5}},
+        {"z", {10.0, 10.0}},
+        {"u", {0.5, -0.5}},
+        {"v", {0.0, 0.0}},
+        {"w", {0.0, 0.0}},
+        {"type", {0, 0}}
+    };
+    testCaseDict["-ic-two-pass2"] = {
+        {"time", {50.0}},
+        {"numParticles", {2}},
+        {"x", {8.5, 11.5}},
+        {"y", {11.3, 8.7}},
+        {"z", {10.0, 10.0}},
+        {"u", {0.5, -0.5}},
+        {"v", {0.0, 0.0}},
+        {"w", {0.0, 0.0}},
+        {"type", {0, 0}}
+    };
+    testCaseDict["-ic-two-pass3"] = {
+        {"time", {50.0}},
+        {"numParticles", {2}},
+        {"x", {8.5, 11.5}},
+        {"y", {11.3, 8.7}},
+        {"z", {10.0, 10.0}},
+        {"u", {0.5, -0.5}},
+        {"v", {0.0, 0.0}},
+        {"w", {0.0, 0.0}},
+        {"type", {1, 1}}
+    };
+    
 
     // test reading in args properly
     while (i < argc) {  //argc num args provided
         cout << "Argument " << i + 1 << ": " << argv[i]
              << endl;
+        if (testCaseDict.find(argv[i]) != testCaseDict.end()) {
+
+        }
+
+        }
         i++;
-    }
     
-    map<char, double> testCaseDict;
-   // testCaseDict['-ic-one'] = [1.0,10,10,10,0.0,0.0,0.0,0];
-
-
 
     return 0;
 }
